247a581974315ccec5399e5f367874273a30b4cc
Michael Vela <mvelajr52@gmail.com>
2023-07-24 17:20:09 -0500

    project update

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const homeUrl = "http://localhost:3000"
-const gamesUrl = homeUrl + "/games"
+const homeURL = "http://localhost:3000"
+const gamesURL = homeURL + "/games"
+const gameList = document.getElementById("list-of-games")
+const gameDetail = document.getElementById("game-detail")
+//const gameImage = document.getElementById("game-image")
+const gameName = document.getElementById("game-name")
+const gameLikes = document.getElementById("display-likes")
+const gameDescription = document.getElementById("display-description")
 
 function renderGame() {
-    fetch(gamesUrl)
+    fetch(gamesURL)
         .then(r => r.json())
         .then(games => {
             games.forEach(displayGames)
         })
 }
+
 renderGame()
+
+
 function displayGames(games) {
-    const gameDetail = document.getElementById("game-detail")
-    const gameImage = document.getElementById("game-image")
-    const gameName = document.getElementById("game-name")
-    const gameList = document.getElementById("list-of-games")
-    const gameRating = document.getElementById("display-rating")
-
-    const image = document.createElement("img")
-    image.src = games.image
-    gameList.appendChild(image)
-
-
-
-
+    const img = document.createElement("img")
+    img.src = games.image
+    gameList.appendChild(img)
+   
+    gameDetail.textContent = games.detail
+    gameName.textContent = games.name
+    gameDescription.textContent = games.description
+    gameLikes.textContent = games.likes
 }
-
