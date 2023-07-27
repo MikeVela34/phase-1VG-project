c4d7a675eedd9abba52adc45fd96049317d10747
Nial Johnson <nialj707@gmail.com>
2023-07-27 13:22:02 -0700

    updated js click

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let currentGame;
+
 const url = 'http://localhost:3000/games'
-/*const url = 'http://localhost:3000/games'
 const gameList = document.getElementById('game-list')
 const gameImages = document.getElementById('game-images')
 const gameName = document.getElementById('game-name')
@@ -8,8 +8,8 @@ const gameDescription = document.getElementById('game-description')
 const gameLikes = document.getElementById('likes')
 const likeButton = document.getElementById('like-btn')
 const dislikeButton = document.getElementById('dislike-btn')
-*/
-/*fetch(url)
+
+fetch(url)
     .then(r => r.json())
     .then(gameArray => {
         renderGameList(gameArray)
@@ -27,10 +27,12 @@ function renderGameList(gameArray) {
         div.append(img)
         gameList.append(div)
         div.append(pTag)
-        img.addEventListener('event', () => {
-            displayGame(gameArray, game)
 
-        })
+        img.onclick = () => {
+            displayGame(gameArray, game)
+        }
+        img.
+
     }
 }
 
@@ -49,50 +51,21 @@ function addLikes() {
             gameLikes.innerText = `Likes: ${currentGame.likes}`
     })
     console.log('i am being clicked')
-}*/
-
-
-fetch(url)
-    .then(r => r.json)
-    .then(data => {
-        data.forEach(game => gameList(game))
-        displayFood(data[0])
-        addLikes()
-        newComment()
-        newGame()
-
-    })
-const gameList = document.getElementById('game-list')
-function renderGameList(game) {
-    const img = document.createElement('img')
-    const pTag = document.createElement('p')
-    const div = document.createElement('div')
-    pTag.innerText = game.name
-    img.src = game.image
-    div.append(img)
-    div.append(pTag)
-    gameList.append(div)
-    img.addEventListener('click', () => {
-        displayGame(game)
-    })
-}
-const gameImages = document.addEventListener('game-images')
-const gameName = document.getElementById('game-name')
-const gameDescription = document.getElementById('game-description')
-const gameLikes = document.getElementById('likes')
-function displayGame(game) {
-    currentGame = game
-    gameImages.src = game.image
-    gameName.innerText = `Game Name: ${game.name}`
-    gameDescription.innerText = `Description: ${game.description}`
-    gameLikes.innerText = `Likes: ${game.likes}`
 }
 
-function addLikes() {
-    const likeButton = document.getElementById('like-btn')
-    likeButton.addEventListener('click', () => {
-        currentGame.likes++
-        gameLikes.innerText = `Likes: ${currentGame.likes}`
-    })
-}
 
+
+// const gamesUrl = "http://localhost:3000/games"
+
+// fetch(gamesUrl)
+//     .then(response => response.json())
+//     .then(gameArray =>
+//         displayGames(gameArray))
+
+// function displayGames(gameArray) {
+//     for (let game in gameArray) {
+//         const gameImage = document.getElementById("game-images")
+//         const
+
+//     }
+// }
